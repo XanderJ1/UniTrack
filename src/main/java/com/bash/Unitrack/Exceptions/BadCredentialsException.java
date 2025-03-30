010821c50d8e3588a33830582f2ccdd52e4ccc6f
XanderJ1 <bzakariyya6@gmail.com>
2025-03-30 11:13:35 -0500

    Added exceptions and exception handler.

---
+++ src/main/java/com/bash/Unitrack/Exceptions/BadCredentialsException.java
@@ -0,0 +1,8 @@
+package com.bash.Unitrack.Exceptions;
+
+public class BadCredentialsException extends Exception{
+
+    public BadCredentialsException(String message){
+        super(message);
+    }
+}
