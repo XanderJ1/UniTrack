3b1eace0d8f26aa51eaa566927b9116be776dfc3
XanderJ1 <bzakariyya6@gmail.com>
2025-03-30 11:14:24 -0500

    Modified DTOs

---
+++ src/main/java/com/bash/Unitrack/Data/DTO/AttendanceDTO.java
@@ -0,0 +1,23 @@
+package com.bash.Unitrack.Data.DTO;
+
+public class AttendanceDTO {
+    private Long sessionId;
+    private Long studentId;
+
+    public Long getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(Long sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public Long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Long studentId) {
+        this.studentId = studentId;
+    }
+
+}
--- src/main/java/com/bash/Unitrack/Data/DTO/SessionDTO.java
+++ src/main/java/com/bash/Unitrack/Data/DTO/SessionDTO.java
@@ -15,25 +15,5 @@ public class SessionDTO {
         this.course = course;
     }
 
-    public static class AttendanceDTO {
-        private Long sessionId;
-        private Long studentId;
 
-        public Long getSessionId() {
-            return sessionId;
-        }
-
-        public void setSessionId(Long sessionId) {
-            this.sessionId = sessionId;
-        }
-
-        public Long getStudentId() {
-            return studentId;
-        }
-
-        public void setStudentId(Long studentId) {
-            this.studentId = studentId;
-        }
-
-    }
 }
