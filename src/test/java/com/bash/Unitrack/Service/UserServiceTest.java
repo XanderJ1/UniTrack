7c9573c1c9a75a79fd8c513bd5c91cab47b4929c
XanderJ1 <bzakariyya6@gmail.com>
2025-05-12 16:17:26 +0000

    Add test for userservice

---
+++ src/test/java/com/bash/Unitrack/Service/UserServiceTest.java
@@ -0,0 +1,34 @@
+package com.bash.Unitrack.Service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void loadUserByUsername() {
+    }
+
+    @Test
+    void fetchUsers() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
